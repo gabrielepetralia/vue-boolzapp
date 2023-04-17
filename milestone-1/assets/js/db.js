a2ea330d3339d2f3cfedda9f2f0fbc548ce56803
gabrielepetralia <gabri.petralia03@gmail.com>
2023-04-17 14:57:21 +0200

    [add] contacts-col content

--- milestone-1/assets/js/db.js
+++ milestone-1/assets/js/db.js
@@ -1,7 +1,7 @@
 const contacts = [
   {
     name: 'Michele',
-    avatar: './img/avatar_1.png',
+    avatar: 'avatar_1.jpg',
     visible: true,
     messages: [
         {
@@ -23,7 +23,7 @@ const contacts = [
   },
   {
     name: 'Fabio',
-    avatar: './img/avatar_2.png',
+    avatar: 'avatar_2.jpg',
     visible: true,
     messages: [
         {
@@ -45,7 +45,7 @@ const contacts = [
   },
   {
     name: 'Samuele',
-    avatar: './img/avatar_3.png',
+    avatar: 'avatar_3.jpg',
     visible: true,
     messages: [
         {
@@ -67,7 +67,7 @@ const contacts = [
   },
   {
     name: 'Alessandro B.',
-    avatar: './img/avatar_4.png',
+    avatar: 'avatar_4.jpg',
     visible: true,
     messages: [
         {
@@ -84,7 +84,7 @@ const contacts = [
   },
   {
     name: 'Alessandro L.',
-    avatar: './img/avatar_5.png',
+    avatar: 'avatar_5.jpg',
     visible: true,
     messages: [
         {
@@ -101,7 +101,7 @@ const contacts = [
   },
   {
     name: 'Claudia',
-    avatar: './img/avatar_5.png',
+    avatar: 'avatar_6.jpg',
     visible: true,
     messages: [
         {
@@ -123,7 +123,7 @@ const contacts = [
   },
   {
     name: 'Federico',
-    avatar: './img/avatar_7.png',
+    avatar: 'avatar_7.jpg',
     visible: true,
     messages: [
         {
@@ -140,7 +140,7 @@ const contacts = [
   },
   {
     name: 'Davide',
-    avatar: './img/avatar_8.png',
+    avatar: 'avatar_8.jpg',
     visible: true,
     messages: [
         {
