ace28a70d96885bff1cd145aae343566ce976dd6
gabrielepetralia <gabri.petralia03@gmail.com>
2023-04-17 19:20:20 +0200

    [add] messages

--- milestone-1/assets/js/db.js
+++ milestone-1/assets/js/db.js
@@ -5,17 +5,20 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: '10/01/2020 16:15:22',
+        date: '10/01/2020',
+        time: '16:15',
         message: 'Tutto fatto!',
         status: 'received'
       },
       {
-        date: '10/01/2020 15:50:00',
+        date: '10/01/2020',
+        time: '15:50',
         message: 'Ricordati di stendere i panni',
         status: 'sent'
       },
       {
-        date: '10/01/2020 15:30:55',
+        date: '10/01/2020',
+        time: '15:30',
         message: 'Hai portato a spasso il cane?',
         status: 'sent'
       }
@@ -27,17 +30,20 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: '20/03/2020 16:35:00',
+        date: '20/03/2020',
+        time: '16:35',
         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
         status: 'sent'
       },
       {
-        date: '20/03/2020 16:30:55',
+        date: '20/03/2020',
+        time: '16:30',
         message: 'Bene grazie! Stasera ci vediamo?',
         status: 'received'
       },
       {
-        date: '20/03/2020 16:30:00',
+        date: '20/03/2020',
+        time: '16:28',
         message: 'Ciao come stai?',
         status: 'sent'
       }
@@ -49,17 +55,20 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: '28/03/2020 16:15:22',
+        date: '28/03/2020',
+        time: '10:25',
         message: 'Ah scusa!',
         status: 'received'
       },
       {
-        date: '28/03/2020 10:20:10',
+        date: '28/03/2020',
+        time: '10:15',
         message: 'Sicuro di non aver sbagliato chat?',
         status: 'sent'
       },
       {
-        date: '28/03/2020 10:10:40',
+        date: '28/03/2020',
+        time: '10:10',
         message: 'La Marianna va in campagna',
         status: 'received'
       }
@@ -71,12 +80,14 @@ const contacts = [
     visible: true,
     messages: [
         {
-          date: '10/01/2020 15:30:55',
+          date: '10/01/2020',
+          time: '15:30',
           message: 'Lo sai che ha aperto una nuova pizzeria?',
           status: 'sent'
         },
         {
-          date: '10/01/2020 15:50:00',
+          date: '10/01/2020',
+          time: '15:30',
           message: 'Si, ma preferirei andare al cinema',
           status: 'received'
         }
@@ -88,12 +99,14 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: '10/01/2020 15:50:00',
+        date: '10/01/2020',
+        time: '15:50',
         message: 'Va bene, stasera la sento',
         status: 'received'
       },
       {
-        date: '10/01/2020 15:30:55',
+        date: '10/01/2020',
+        time: '15:35',
         message: 'Ricordati di chiamare la nonna',
         status: 'sent'
       }
@@ -105,17 +118,20 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: '10/01/2020 15:51:00',
+        date: '10/01/2020',
+        time: '15:55',
         message: 'Nessuna nuova, buona nuova',
         status: 'sent'
       },
       {
-        date: '10/01/2020 15:50:00',
+        date: '10/01/2020',
+        time: '15:50',
         message: 'Non ancora',
         status: 'received'
       },
       {
-        date: '10/01/2020 15:30:55',
+        date: '10/01/2020',
+        time: '15:30',
         message: 'Ciao Claudia, hai novità?',
         status: 'sent'
       }
@@ -127,12 +143,14 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: '10/01/2020 15:50:00',
+        date: '10/01/2020',
+        time: '15:50',
         message: 'Grazie per avermelo ricordato, le scrivo subito!',
         status: 'received'
       },
       {
-        date: '10/01/2020 15:30:55',
+        date: '10/01/2020',
+        time: '15:30',
         message: 'Fai gli auguri a Martina che è il suo compleanno!',
         status: 'sent'
       }
@@ -144,17 +162,20 @@ const contacts = [
     visible: true,
     messages: [
       {
-        date: '10/01/2020 15:51:00',
+        date: '10/01/2020',
+        time: '15:55',
         message: 'OK!!',
         status: 'received'
       },
       {
-        date: '10/01/2020 15:50:00',
+        date: '10/01/2020',
+        time: '15:50',
         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
         status: 'sent'
       },
       {
-        date: '10/01/2020 15:30:55',
+        date: '10/01/2020',
+        time: '15:30',
         message: 'Ciao, andiamo a mangiare la pizza stasera?',
         status: 'received'
       }
