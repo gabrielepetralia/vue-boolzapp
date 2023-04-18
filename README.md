6d729ba4d22e0e3b85a29ccf50f6ce95921e7098
gabrielepetralia <gabri.petralia03@gmail.com>
2023-04-18 17:13:56 +0200

    [milestone-5] first commit

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Boolzapp
 ## Milestone 1
 - **Replica della grafica** con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
 - **Visualizzazione dinamica della lista contatti**: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+- **Visualizzazione ora e ultimo messaggio inviato/ricevuto** nella lista dei contatti 
 
 ## Milestone 2
 - **Visualizzazione dinamica dei messaggi**: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
@@ -17,3 +18,6 @@ Boolzapp
 
 ## Milestone 4
 - **Ricerca utenti**: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+## Milestone 5
+- **Cancella messaggio**: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
---
+++ milestone-5/assets/js/db.js
@@ -0,0 +1,186 @@
+const contacts = [
+  {
+    name: 'Michele',
+    avatar: 'avatar_1.jpg',
+    id: 0,
+    messages: [
+      {
+        date: '10/01/2020',
+        time: '15:30',
+        message: 'Hai portato a spasso il cane?',
+        status: 'sent'
+      },
+      {
+        date: '10/01/2020',
+        time: '15:50',
+        message: 'Ricordati di stendere i panni',
+        status: 'sent'
+      },
+      {
+        date: '10/01/2020',
+        time: '16:15',
+        message: 'Tutto fatto!',
+        status: 'received'
+      }
+    ],
+  },
+  {
+    name: 'Fabio',
+    avatar: 'avatar_2.jpg',
+    id: 1,
+    messages: [
+      {
+        date: '20/03/2020',
+        time: '16:28',
+        message: 'Ciao come stai?',
+        status: 'sent'
+      },
+      {
+        date: '20/03/2020',
+        time: '16:30',
+        message: 'Bene grazie! Stasera ci vediamo?',
+        status: 'received'
+      },
+      {
+        date: '20/03/2020',
+        time: '16:35',
+        message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+        status: 'sent'
+      }
+    ],
+  },
+  {
+    name: 'Samuele',
+    avatar: 'avatar_3.jpg',
+    id: 2,
+    messages: [
+      {
+        date: '28/03/2020',
+        time: '10:10',
+        message: 'La Marianna va in campagna',
+        status: 'received'
+      },
+      {
+        date: '28/03/2020',
+        time: '10:15',
+        message: 'Sicuro di non aver sbagliato chat?',
+        status: 'sent'
+      },
+      {
+        date: '28/03/2020',
+        time: '10:25',
+        message: 'Ah scusa!',
+        status: 'received'
+      }
+    ],
+  },
+  {
+    name: 'Alessandro B.',
+    avatar: 'avatar_4.jpg',
+    id: 3,
+    messages: [
+      {
+        date: '10/01/2020',
+        time: '15:30',
+        message: 'Lo sai che ha aperto una nuova pizzeria?',
+        status: 'sent'
+      },
+      {
+        date: '10/01/2020',
+        time: '15:35',
+        message: 'Si, ma preferirei andare al cinema',
+        status: 'received'
+      }
+    ],
+  },
+  {
+    name: 'Alessandro L.',
+    avatar: 'avatar_5.jpg',
+    id: 4,
+    messages: [
+      {
+        date: '10/01/2020',
+        time: '15:35',
+        message: 'Ricordati di chiamare la nonna',
+        status: 'sent'
+      },
+      {
+        date: '10/01/2020',
+        time: '15:50',
+        message: 'Va bene, stasera la sento',
+        status: 'received'
+      }
+    ],
+  },
+  {
+    name: 'Claudia',
+    avatar: 'avatar_6.jpg',
+    id: 5,
+    messages: [
+      {
+        date: '10/01/2020',
+        time: '15:30',
+        message: 'Ciao Claudia, hai novità?',
+        status: 'sent'
+      },
+      {
+        date: '10/01/2020',
+        time: '15:50',
+        message: 'Non ancora',
+        status: 'received'
+      },
+      {
+        date: '10/01/2020',
+        time: '15:55',
+        message: 'Nessuna nuova, buona nuova',
+        status: 'sent'
+      }
+    ],
+  },
+  {
+    name: 'Federico',
+    avatar: 'avatar_7.jpg',
+    id: 6,
+    messages: [
+      {
+        date: '10/01/2020',
+        time: '15:30',
+        message: 'Fai gli auguri a Martina che è il suo compleanno!',
+        status: 'sent'
+      },
+      {
+        date: '10/01/2020',
+        time: '15:50',
+        message: 'Grazie per avermelo ricordato, le scrivo subito!',
+        status: 'received'
+      }
+    ],
+  },
+  {
+    name: 'Davide',
+    avatar: 'avatar_8.jpg',
+    id: 7,
+    messages: [
+      {
+        date: '10/01/2020',
+        time: '15:30',
+        message: 'Ciao, andiamo a mangiare la pizza stasera?',
+        status: 'received'
+      },
+      {
+        date: '10/01/2020',
+        time: '15:50',
+        message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+        status: 'sent'
+      },
+      {
+        date: '10/01/2020',
+        time: '15:55',
+        message: 'OK!!',
+        status: 'received'
+      }
+    ],
+  }
+]
+
+export {contacts}
